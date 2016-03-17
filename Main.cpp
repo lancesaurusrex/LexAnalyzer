369485c37de0a9a82b9ca37ccb0e1b1db157f569
lancesaurusrex <dominatesean@yahoo.com>
2016-03-17 15:49:07 -0700

    ParserWorking

--- Main.cpp
+++ Main.cpp
@@ -15,6 +15,10 @@ bool Definition();
 bool DataDef();
 bool FuncDef();
 bool Type();
+bool Expression();
+bool Identifier();
+bool IODef();
+bool Statement();
 
 int main() 
 {
@@ -91,10 +95,13 @@ int main()
 	for (std::string line; std::getline(infile, line); )
 	{
 		Lexic.String2TokenSequence(line);	
-		//prsea here
+
 	}
 	currentToken = Lexic.cbegin();
-	if (Program(Lexic)){}
+	if (Program(Lexic)){
+		Program(Lexic);
+		cout << "Parse great success!" << '\n' << '\n';
+	}
 
 	for (std::string line; std::getline(infile1, line);)
 	{
@@ -132,11 +139,13 @@ bool Program(CLexicalAnalyzer &c)
 
 	if (Definition())
 	{
-		cout << "Definition =>Statement Statlist" << endl;
-		if (Definition()){
-			
-			return true;
+		if (currentToken->getTokenName() == "$") {
+			cout << "Definition =>Data def|Funct def" << '\n';
+			currentToken++;
+			Program(c);
 		}
+		else
+			cout << "expecting ;" << '\n';
 	}
 	else
 		return false;
@@ -150,14 +159,29 @@ bool Definition()
 	//If true goes into brackets
 	if (DataDef())
 	{
-		Definition();
+		if (currentToken->getTokenName() == ";") {
+			cout << "Data Def => Type Expr ;" << '\n';
+			currentToken++;
+			return true;
+		}
 	}
 
 	if (FuncDef())
 	{
-
+		
 	}
-	return true;
+
+	if (IODef())
+	{
+		cout << "IO IOSym Exp" << '\n';
+		return true;
+	}
+
+	if (Statement()){
+		cout << "StatementDef => { Exp } || if ( EXP ) Statement";
+		return true;
+	}
+	return false;
 }
 
 //enum { nilT, idT, symbolT, operatorT, preT, intT, decimalT, stringT, keyT, errorT, endlineT};
@@ -165,10 +189,14 @@ bool DataDef() {
 	list<CToken>::const_iterator temp;
 	temp = currentToken;
 
-	if (Type())
+	if (Type()) {
+		cout << "Type => int|double|string" << '\n';
 		return true;
-	else
-		return false;
+	}
+
+	if (Expression()) {
+		cout << "DD => Expr";
+	}
 }
 
 bool FuncDef() {
@@ -185,8 +213,10 @@ bool Type() {
 	if (currentToken->getTokenType() == 8) {
 		if (currentToken->getTokenName() == "int" || currentToken->getTokenName() == "double" || currentToken->getTokenName() == "string") {
 			currentToken++;
-			if (Expression())
+			if (Expression()) {
+				cout << "Expr => ID Assignsym Expr|ID Comp_sym Expr|ID_Next" << '\n';
 				return true;
+			}
 		}
 	}
 
@@ -196,27 +226,174 @@ bool Type() {
 bool string_is_valid(const std::string &str)
 {
 	return find_if(str.begin(), str.end(),
-		[](char c) { return !(isalnum(c) || (c == ' ')); }) == str.end();
+		[](char c) { return !(isalnum(c)); }) == str.end();
 }
 
 bool Expression() {
-	if (Indentifer()){//id sym exp}
+	if (Identifier()) {
 
-	if (IDNext()) {}
+		if (Identifier()) {
+
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool Identifier() {
+	list<CToken>::const_iterator temp;
+	temp = currentToken;
+	//Identifer assignsym || Identifier compsym
+
+	//check for id and then peek ahead for sym and check
+	if (currentToken->getTokenType() == 1 && (string_is_valid(currentToken->getTokenName())) ){
+
+		temp++;		//peek ahead to symbol
+		if (temp->getTokenType() == 3) {
+
+			if (temp->getTokenName() == "=" || temp->getTokenName() == "+" ||
+				temp->getTokenName() == "-" || temp->getTokenName() == "*" || temp->getTokenName() == "/") {
+
+				cout << "ID Assignsym ";
+				currentToken++;	//move forward from id
+				currentToken++;	//move forward from assignsym;
+				return true;
+			}
+
+			if (temp->getTokenName() == "!=" || temp->getTokenName() == "==" ||
+				temp->getTokenName() == "<"  || temp->getTokenName() == ">" ||
+				temp->getTokenName() == "<=" || temp->getTokenName() == ">=") {
+
+				cout << "ID CompSym ";
+				currentToken++;	//move forward from id
+				currentToken++;	//move forward from compsym;
+				return true;
+			}
+		}
+		//is id but peek ahead not a sym
+		else {
+			currentToken++;	//move forward from id, next statement not sym
+			return true;
+		}
+
+	}
+	//not an id, ITS A string
+	if (currentToken->getTokenType() == 7 && (string_is_valid(currentToken->getTokenName())))
+	{
+			currentToken++;	//go forward past string
+
+			cout << "string" << '\n';
+			return true;
+		
+	}
+
+	if ((string_is_valid(currentToken->getTokenName()))) {
+		if (currentToken->getTokenType() == 5) {
+
+			currentToken++;
+
+			cout << "int" << '\n';
+			return true;
+		}
+	}
+	//decimals wont pass the string_is_valid test
+	if ( currentToken->getTokenType() == 6){ 
+		currentToken++;
+
+	cout << "double" << '\n';
+	return true;
+}
+	return false;
+
 
 }
 
-bool Indentifer() {
+bool IODef() {
+
 	list<CToken>::const_iterator temp;
 	temp = currentToken;
 
-	if ((string_is_valid(currentToken->getTokenName))){
-		//is id
+	if (currentToken->getTokenType() == 8){
+		if (currentToken->getTokenName() == "cin" || currentToken->getTokenName() == "cout") {
+			temp++;
+			bool extLoop = true;
+
+			while (extLoop == true){
+				if (temp->getTokenType() == 3) {
+					if (temp->getTokenName() == "<<" || temp->getTokenName() == ">>") {
+						temp++;
+						currentToken++;	//go forward over keyword
+						currentToken++;	//go forward over extractor
+
+						if (Identifier()) {
+							temp++;	//peek for extractor or ;
+							if (temp->getTokenName() == ";") {
+								currentToken++;
+								return true;
+							}
+								
+						}
+						else {
+							cout << "Can't find ; at end of IO IOSYM ID" << '\n';
+							return false;
+						}
+					}
+				}
+			}
+		}
+	}
+	return false;
+}
+
+bool Statement() {
+
+	list<CToken>::const_iterator temp;
+	temp = currentToken;
+
+	if (currentToken->getTokenName() == "if") {
+		++currentToken;	//forward off of if
+		if (currentToken->getTokenName() == "(") {
+			++currentToken;	//forward off of (
+			if (Expression()) {
+				if (currentToken->getTokenName() == ")") {
+					currentToken++;
+					if (Statement()) {
+						cout << "if ( Expression ) Statement " << '\n';
+						return true;
+					}
+				}
+				else
+					cout << "No ) in" << temp->getTokenName() << "Statement" << '\n';
+			}
+		}
+		else
+			cout << "No ( in" << temp->getTokenName() << "Statement" << '\n';
+	}
+
+	if (currentToken->getTokenName() == "{"){
 		currentToken++;
-		if (AssignSym()){//is assignsym gobacktoxpr}
-		else if (CompSym()){//is compsym go \backtoxpr}
-		else//idnext
+		temp++;
+		if (temp->getTokenType() == 8) //peek next, if keyword go to type
+			Type();
+		else
+			Expression();
+		if (currentToken->getTokenName() == ";")
+			currentToken++;
+		else
+			cout << "No ; at the end";
+		if (currentToken->getTokenName() == "}")
+		{
+			currentToken++;
+			cout << "{ Expression }" << '\n';
+			return true;
+		}
+		else
+			cout << "No { before " << temp->getTokenName() << " Expression" << '\n';
+
 	}
 	else
-		return false;
+		cout << "No } after " << temp->getTokenName() << " Expression" << '\n';
+
+	return false;
 }
